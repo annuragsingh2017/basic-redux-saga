f9f5056faa0642d675ac148c1d46fd501b8352ad
annuragsingh2017 <annuragsingh2017@gmail.com>
2022-06-30 14:46:13 +0530

    basic components changes

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import UserComponant from "./componants/UserComponant";
+
+function App() {
+  return (
+    <div>
+      jdfsklghsfbj
+      <UserComponant />
+    </div>
+  );
+}
+
+export default App;
