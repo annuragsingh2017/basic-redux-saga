b93c3205dfd47b761dab3e8330fdf2982d1080c1
annuragsingh2017 <annuragsingh2017@gmail.com>
2022-06-30 14:56:02 +0530

    Redux-Types

---
+++ src/redux/types.js
@@ -0,0 +1,4 @@
+export const USER_TYPE = "user_type";
+export const GET_USER_API_DATA = "GET_USER_API_DATA";
+export const USER_DATA_FAILED = "USER_DATA_FAILED";
+export const GET_USER_REQUEST = "GET_USER_REQUEST";
